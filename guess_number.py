03f3c13c6de5ea53f01af961cdf6f956005fa942
Константин <koctella231@gmail.com>
2024-11-14 17:31:16 +0300

    Несколько интересных кодов

--- guess_number.py
+++ guess_number.py
@@ -1,24 +1,95 @@
-# Импорт функции получения случайных чисел
-# из модуля random.
-from random import randint
+# class BacteriaProducer:
+#     maximum_bacteria_now = 0
+#     # Допишите инициализатор класса 
+#     def __init__(self, max_bacteria):
+#         self.bacteria_count = 0  # Изначально количество бактерий равно 0
+#         self.max_bacteria = max_bacteria  # Максимально возможное количество бактерий
 
-# Получаем случайное число в диапазоне от 1 до 100.
-number = randint(1, 100)
-print('Угадайте число от 1 до 100')
+#     def create_new(self):
+#         if self.bacteria_count < self.max_bacteria:
+#             self.bacteria_count += 1  # Увеличиваем счетчик бактерий на 1
+#             print(f'Добавлена одна бактерия. Количество бактерий в популяции: {self.bacteria_count}')
+#         else:
+#             print('Нет места под новую бактерию')
+#     # Допишите метод
+#     def remove_one(self):
+#         if self.bacteria_count > 0:
+#             self.bacteria_count -= 1  # Уменьшаем счетчик бактерий на 1
+#             print(f'Одна бактерия удалена. Количество бактерий в популяции: {self.bacteria_count}')
+#         else:
+#             print('В популяции нет бактерий, удалять нечего')
 
-while True:
-    # Получаем число от пользователя и сохраняем его в переменную.
-    guess = int(input('Введите число: '))
+# # Пример запуска для самопроверки
+# bacteria_producer = BacteriaProducer(max_bacteria=3)
+# bacteria_producer.remove_one()
+# bacteria_producer.create_new()
+# bacteria_producer.create_new()
+# bacteria_producer.create_new()
+# bacteria_producer.create_new()
+# bacteria_producer.remove_one()
 
-    # Если число меньше загаданного...
-    if guess < number:
-        # ...выводим сообщение.
-        print('Ваше число меньше того, что загадано.')
-    elif guess > number:
-        # ...выводим сообщение.
-        print('Ваше число больше того, что загадано.')
-    elif guess == number:
-        # ...прерываем выполнение программы и...
-        break
-# ...выводим сообщение.
-print('Отличная интуиция! Вы угадали число :)')
+
+
+
+
+# class MushroomsCollector:
+#     def __init__(self):
+#         self.mushrooms = []  # Изначально корзина пуста
+
+#     def is_poisonous(self, mushroom_name):
+#         # Пример проверки на ядовитость
+#         poisonous_mushrooms = ['мухомор', 'поганка']  # Список ядовитых грибов
+#         return mushroom_name.lower() in poisonous_mushrooms
+
+#     def add_mushroom(self, mushroom_name):
+#         if self.is_poisonous(mushroom_name):
+#             print('Нельзя добавить ядовитый гриб')
+#         else:
+#             self.mushrooms.append(mushroom_name)  # Добавляем гриб в корзину
+#             print(f'Гриб {mushroom_name} добавлен в корзину')
+
+#     def __str__(self):
+#         return ', '.join(self.mushrooms) 
+# # Пример запуска для самопроверки
+# collector_1 = MushroomsCollector()
+# collector_1.add_mushroom('Мухомор')
+# collector_1.add_mushroom('Подосиновик')
+# collector_1.add_mushroom('Белый')
+# print(collector_1)
+
+# collector_2 = MushroomsCollector()
+# collector_2.add_mushroom('Лисичка')
+# print(collector_1)
+# print(collector_2)
+
+
+
+# class CipherMaster:
+#     alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+#     def cipher(self, original_text, shift):
+#         result = []
+#         for letter in original_text:
+#             position = self.alphabet.find(letter.lower())
+#             if position == -1:
+#                 result.append(letter)
+#             else:
+#                 result.append(self.alphabet[(position + shift) % len(self.alphabet)])
+#         return ''.join(result)
+#     def decipher(self, cipher_text, shift):
+#         result = []
+#         for letter in cipher_text:
+#             position = self.alphabet.find(letter.lower())
+#             if position == -1:
+#                 result.append(letter)
+#             else:
+#                 result.append(self.alphabet[(position - shift) % len(self.alphabet)])
+#         return ''.join(result)
+# cipher_master = CipherMaster()
+# print(cipher_master.cipher(
+#     original_text='Однажды ревьюер принял проект с первого раза, с тех пор я его боюсь',
+#     shift=2
+# ))
+# print(cipher_master.decipher(
+#     cipher_text='Олебэи яфвнэ мроплж сэжи — э пэй рдв злййвкпш лп нвящывнэ',
+#     shift=-3
+# ))
